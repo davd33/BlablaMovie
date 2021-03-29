6b8369af7404dec1424ba36b9c9e6538fed5e441
David Rueda <davd33@gmail.com>
2021-03-29 18:38:03 +0200

    some front end
    search a movie and display all results with some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [movies, setMovies] = useState([]);
+
+    const handleMovieSearch = (e) => axios
+          .get(encodeURI(`http://localhost:3001/find-movie?movieName=${e.target.value}`))
+          .then(r => setMovies(r.data.Search || movies));
+
+    return (
+        <div className="App">
+          <input type="text" placeholder="search movie" onChange={handleMovieSearch} />
+
+          <div>
+            {movies.length < 1 ? "No movies found..." : ""}
+            <ul className="moviesFound">
+              {movies.map(m => <li key={m.imdbID}><img src={m.Poster} /><h1>{m.Title} ({m.Year})</h1></li>)}
+            </ul>
+          </div>
+        </div>
+    );
 }
 
 export default App;
