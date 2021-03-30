2dff3d2b93c70db100b593f1b6ad6f7b4a7f6d2e
David Rueda <davd33@gmail.com>
2021-03-30 14:24:13 +0200

    movies component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
-    const [movies, setMovies] = useState([]);
-
-    const handleMovieSearch = (e) => axios
-          .get(encodeURI(`http://localhost:3001/find-movie?movieName=${e.target.value}`))
-          .then(r => setMovies(r.data.Search || movies));
-
     return (
         <div className="App">
-          <input type="text" placeholder="search movie" onChange={handleMovieSearch} />
 
-          <div>
-            {movies.length < 1 ? "No movies found..." : ""}
-            <ul className="moviesFound">
-              {movies.map(m => <li key={m.imdbID}><img src={m.Poster} /><h1>{m.Title} ({m.Year})</h1></li>)}
-            </ul>
-          </div>
         </div>
     );
 }
---
+++ frontend/src/components/movies/Movies.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import './Movies.css';
+
+function Movies() {
+
+    const [movies, setMovies] = useState([]);
+
+    const m = {"/clear": () => setMovies([])};
+    const handleMovieSearch = (e) => m[e.target.value] ? m[e.target.value](e) : axios
+          .get(encodeURI(`http://localhost:3001/find-movie?movieName=${e.target.value}`))
+          .then(r => { if (r.data.Search) setMovies(r.data.Search); });
+
+    const voteForMovie = (movieId) => {console.log('vote '+ movieId);};
+
+    return (
+        <div>
+          <input type="text" placeholder="search movie" onChange={handleMovieSearch} />
+
+          <div>
+            {movies.length < 1 ? "No movies found..." : ""}
+            <ul className="movies-found">
+              {movies.map((m, i) => <li key={m.imdbID+i} onClick={() => voteForMovie(m.imdbID)} className="movie">
+                                      <img src={m.Poster} />
+                                      <h1>{m.Title} ({m.Year})</h1>
+                                    </li>)}
+            </ul>
+          </div>
+        </div>
+    );
+}
+
+export default App;
